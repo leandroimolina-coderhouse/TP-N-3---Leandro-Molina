8b3d3edbbb0f6e4b4979afe8397242e14d586760
Leandro Molina <leandroimolina@gmail.com>
2025-12-23 02:10:20 -0300

    Finalizado

--- hospital/models.py
+++ hospital/models.py
@@ -1,11 +1,1 @@
 from django.db import models
-
-class DepartamentoMedico(models.Model):
-    nombre = models.nombre(max_lenght=50)
-    nro_departamento = models.IntegerField(unique=True)
-    nro_empleados = models.IntegerField(null=True)
-    fecha_de_creacion = models.fecha_de_creacion(auto_now_add=true)
-    email_dpto = models.email_dpto() 
-
-    def __str__(self):
-        return f"Nombre: {self.nombre} / Nro_depto: {self.nro_departamento}"
